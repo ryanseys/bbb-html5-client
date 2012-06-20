8d489a8bb0a147b32f28d7d965abcf63861d41a2
Ryan Seys <rsmseys@gmail.com>
2012-06-20 09:24:24 -0400

    June 20 start

--- public/javascripts/chat/whiteboard.js
+++ public/javascripts/chat/whiteboard.js
@@ -20,6 +20,7 @@ var lineOn;
 //for lines
 var path;
 var step;
+var end;
 
 //for rectangles
 var rect;
@@ -67,7 +68,6 @@ function initDefaults() {
   
   slide = defaults[0];
   cur = defaults[1];
-  turnOnShape("line");
 }
 
 function initEvents() {
@@ -75,52 +75,54 @@ function initEvents() {
   slide.mousemove(mvCur);
   
   //when dragging
-  //cur.drag(curDragging, curDragStart, curDragStop); //for lines
-  cur.drag(curRectDragging, curRectDragStart, curRectDragStop); //for rectangles
+  cur.drag(curDragging, curDragStart, curDragStop); //for lines
+  //cur.drag(curRectDragging, curRectDragStart, curRectDragStop); //for rectangles
 }
 
 var curDragStart = function(x, y, e) {
-  p_curx = e.offsetX;
-  p_cury = e.offsetY;
-  path = "M" + p_curx + " " + p_cury + "L" + p_curx + " " + p_cury;
+    p_curx = e.offsetX;
+    p_cury = e.offsetY;
+    path = "M" + p_curx + " " + p_cury + "L" + p_curx + " " + p_cury;
 };
 
 var curDragging = function(dx, dy, x, y, e) {
-  curx = e.offsetX;
-  cury = e.offsetY;
-  step = "M" + p_curx + " " + p_cury + "L" + curx + " " + cury;
-  paper.path(step);
-  path += "L" + curx + " " + cury;
-  p_curx = curx;
-  p_cury = cury;
+    curx = e.offsetX;
+    cury = e.offsetY;
+    end = "L" + curx + " " + cury;
+    step = "M" + p_curx + " " + p_cury + end;
+    paper.path(step);
+    path += end;
+    p_curx = curx;
+    p_cury = cury;
 };
 
 var curDragStop = function(e) {
-  curves = Raphael.path2curve(path);
+    curves = Raphael.path2curve(path);
 };
 
 var curRectDragStart = function(x, y, e) {
-  p_curx = e.offsetX;
-  p_cury = e.offsetY;
-  rect = paper.rect(p_curx, p_cury, 0, 0);
+    p_curx = e.offsetX;
+    p_cury = e.offsetY;
+    rect = paper.rect(p_curx, p_cury, 0, 0);
 };
 
 var curRectDragging = function(dx, dy, x, y, e) {
-  if(dx < 0) {
-    xval = p_curx + dx;
-    dx = -dx;
-  }
-  else xval = p_curx;
-  if(dy < 0) {
-    yval = p_cury + dy;
-    dy = -dy;
-  }
-  else yval = p_cury;
-  rect.attr({ x: xval, y: yval, width: dx, height: dy});
+    if(dx < 0) {
+      xval = p_curx + dx;
+      dx = -dx;
+    }
+    else xval = p_curx;
+    if(dy < 0) {
+      yval = p_cury + dy;
+      dy = -dy;
+    }
+    else yval = p_cury;
+    rect.attr({ x: xval, y: yval, width: dx, height: dy});
 };
 
 var curRectDragStop = function(e) {
-  console.log('done rectangle');
+
+    console.log('done rectangle');
 };
 
 var mvCur = function(e) {
